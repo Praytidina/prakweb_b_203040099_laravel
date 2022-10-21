8ce31004a122477a0e8abc50cf8e502bd2f6bee5
raypratidina <raypz642@gmail.com>
2022-10-21 20:18:48 +0700

    menyelesaikan video 4

--- routes/web.php
+++ routes/web.php
@@ -14,19 +14,82 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/ ', function () {
-    return view('Home');
+    return view('Home', [
+        "title" => "Home"
+    ]);
 });
+
+
 
 
 Route::get('/about ', function () {
     return view('about', [
+        "title" => "about",
         "name" => "Ray pratidina",
         "email" => "Raypz642@gmail.com.id",
         "image" => "hapus.jpg"
     ]);
 });
 
-Route::get('/posts ', function () {
-    return view('posts');
+Route::get('/blog ', function () {
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Ray Pratidina",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur inventore minus fuga omnis est hic perspiciatis, reiciendis itaque similique tenetur."
+            
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Farhan abas",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, quod labore minima ex numquam, expedita commodi modi a qui, debitis iste iusto ut. Tempora repudiandae, modi quaerat libero nihil officia."
+            
+        ]
+    ]; 
+    
+    return view('posts', [
+        "title" => "Posts",
+        "posts" => $blog_posts
+
+    ]);
 });
+
+
+///singlepost
+
+Route::get('posts/{slug}', function($slug) {
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Ray Pratidina",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur inventore minus fuga omnis est hic perspiciatis, reiciendis itaque similique tenetur."
+            
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Farhan abas",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, quod labore minima ex numquam, expedita commodi modi a qui, debitis iste iusto ut. Tempora repudiandae, modi quaerat libero nihil officia."
+            
+        ]
+    ]; 
+$new_post= [];
+foreach($blog_posts as $post) {
+    if($post["slug"] === $slug) {
+        $new_post = $post;
+
+    }
+}
+
+    return view('post', [
+        "title" => "single post",
+        "post " => $new_post
+    ]);
+});
+
+
+
   
