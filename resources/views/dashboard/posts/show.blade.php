1ceb945e23aff4948499483c96b91c314348e1b5
raypratidina <raypz642@gmail.com>
2022-11-05 11:42:55 +0700

    menyelesaikan video 17

---
+++ resources/views/dashboard/posts/show.blade.php
@@ -0,0 +1,28 @@
+@extends('dashboard.layouts.main')
+
+@section('container')
+<div class="container">
+        <div class="row my-3">
+            <div class="col-lg-8">
+            <h1 class="mb-3">{{ $post->title }}</h1>
+
+            <a href="/dashboard/posts" class="btn btn-success"><span daya-feather="arrow-left"></span>Back to my posts</a>
+            <a href="" class="btn btn-warning"><span daya-feather="edit"></span>Edit</a>
+            <a href="" class="btn btn-danger"><span daya-feather="x-circle">
+            </span>Delete</a>
+
+
+            <img src ="https://source.unsplash.com/1200x400/?{{ $post->category->name }}" alt="{{ $post->category->name }}" class="img-fluid">
+
+
+        <article class="my-3 fs-5">
+
+        {!! $post->body !!}
+
+        </article>
+
+            </div>
+        </div>
+    </div>
+
+@endsection
