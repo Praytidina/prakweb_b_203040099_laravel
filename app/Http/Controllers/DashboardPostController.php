1ff0e1b8171c519fac69bec0c36454239360e210
raypratidina <raypz642@gmail.com>
2022-11-27 13:49:15 +0700

    DELETE

--- app/Http/Controllers/DashboardPostController.php
+++ app/Http/Controllers/DashboardPostController.php
@@ -100,7 +100,9 @@ class DashboardPostController extends Controller
      */
     public function destroy(Post $post)
     {
-        //
+        Post::destroy($post->id);
+
+        return redirect('/dashboard/posts')->with('success', 'post has been deleted!');
     }
 
     public function checkSlug(Request $request)
