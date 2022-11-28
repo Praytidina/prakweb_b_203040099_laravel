f351c941639cef200af0c03ab442528268e6b90b
raypratidina <raypz642@gmail.com>
2022-11-28 09:34:45 +0700

    meneyelesaikan video 22

--- resources/views/dashboard/posts/edit.blade.php
+++ resources/views/dashboard/posts/edit.blade.php
@@ -5,7 +5,7 @@
         <h1 class="h2">Edit Post</h1>
 </div>
 <div class="col-lg-8">
-<form method="post" action="/dashboard/posts/{{ $post->slug }}" class="mb-5">
+<form method="post" action="/dashboard/posts/{{ $post->slug }}" class="mb-5" enctype="multipart/form-data">
   @method('put')
     @csrf
   <div class="mb-3">
@@ -13,7 +13,7 @@
     <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" required autofocus value="{{ old('title', $post->title) }}">
     @error('title')
        <div class="invalid-feedback">
-        {{ $messsage }} 
+        {{ $message }} 
     @enderror
   </div>
   <div class="mb-3">
@@ -21,13 +21,13 @@
     <input type="text" class="form-control @error('slug') is-invalid @enderror" id="slug" name="slug" required value="{{ old('slug', $post->slug) }}">
     @error('slug')
        <div class="invalid-feedback">
-        {{ $messsage }} 
+        {{ $message }} 
     @enderror
   </div>
   <div class="mb-3">
     <label for="category" class="form-label">Category</label>
     @error('body')
-        <p class="text-danger">{{ message }}</p>
+        <p class="text-danger">{{ $message }}</p>
     @enderror
     <select class="form-select" name="category_id">
     @foreach ($categories as $category)
@@ -40,6 +40,24 @@
     @endforeach
 </select>
 </div>
+
+<div class="mb-3">
+  <label for="image" class="form-label">Post image</label>
+  <input type="hidden" name="oldImage" value="{{ $post->image }}">
+  @if($post->image)
+  <img src="{{ asset('storage/' . $post->image) }}"class="img-preview img-fluid mb-3 col-sm-5 d-block">
+  @else
+  <img class="img-preview img-fluid mb-3 col-sm-5">
+  @endif
+  
+  <input class="form-control @error('image') is-invalid @enderror" type="file" id="image" name="image" onchange="previewImage()">
+  @error('image')
+       <div class="invalid-feedback">
+        {{ $message }} 
+    </div>
+    @enderror
+</div>
+
   <div class="mb-3">
     <label for="body" class="form-label">Body</label>
   <input id="body" type="hidden" name="body" value="{{ old('body', $post->body ) }}">
@@ -65,5 +83,19 @@
   document.addEventlistener('tirx-file-accept', function(e) {
     e.preventDefault();
   })
+
+  function previewImage() {
+    const image = document.querySelector('#image');
+    const imgPreview = document.querySelector('.img-preview');
+
+    imgPreview.style.display = 'block';
+
+    const oFReader = new FileReader();
+    oFReader.readAsDataURL(image.files[0]);
+
+    oFReader.onload = function(oFREvent) {
+        imgPreview.src = oFREvent.target.result;
+    }
+  }
 </script>
 @endsection
